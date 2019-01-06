56f4ae6ed98518e6a9b13300c68d3a7dda9f3cda
Maria <marialebed@mail.ru>
2019-01-06 17:35:17 +0300

    Готовый проект

---
+++ js/scripts.js
@@ -0,0 +1,81 @@
+var buy = document.querySelectorAll(".btn-buy");
+var cart = document.querySelector(".modal-cart");
+var close = cart.querySelector(".modal-close");
+
+for (var i = 0; i < buy.length; i++) {
+  buy[i].addEventListener("click",function(evt) {
+    event.preventDefault();
+    cart.classList.add("modal-show");
+  })
+};
+
+close.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  cart.classList.remove("modal-show");
+});
+
+var link = document.querySelector(".message-link");
+var popup = document.querySelector(".modal");
+var close = popup.querySelector(".modal-close");
+var form = popup.querySelector("form");
+var name = popup.querySelector("[name=name]");
+var email = popup.querySelector("[name=email]");
+var isStorageSupport = true;
+var storage = "";
+
+try {
+  storage = localStorage.getItem("name");
+} catch (err) {
+  isStorageSupport = false;
+}
+
+link.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popup.classList.add("modal-show");
+  if (storage) {
+    name.value = storage;
+    email.focus();
+  } else {
+    name.focus();
+  }
+});
+
+close.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popup.classList.remove("modal-show");
+});
+
+form.addEventListener("submit", function (evt) {
+  if (!name.value || !email.value) {
+    evt.preventDefault();
+    console.log("Введите имя и адрес электронной почты");
+  } else {
+    if (isStorageSupport) {
+      localStorage.setItem("name", name.value);
+    }
+  }
+});
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    evt.preventDefault();
+  if (popup.classList.contains("modal-show")) {
+    popup.classList.remove("modal-show");
+    }
+  }
+});
+
+
+var mapLink = document.querySelector(".show-map");
+var mapPopup = document.querySelector(".modal-map");
+var mapClose = mapPopup.querySelector(".modal-close");
+
+mapLink.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  mapPopup.classList.add("modal-show");
+});
+
+mapClose.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  mapPopup.classList.remove("modal-show");
+});
